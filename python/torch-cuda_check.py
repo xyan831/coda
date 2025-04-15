f7b83094ec31815040c947ae054f4164c093cd62
颜筱辰 Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2025-04-15 12:18:42 -0700

    Add files via upload

---
+++ python/torch-cuda_check.py
@@ -0,0 +1,7 @@
+import torch
+
+# install torch for cuda 12.2
+# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
+# check if cuda available
+print(torch.cuda.is_available())
