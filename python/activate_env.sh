28156682ec7f03a9aab93c6ef25da337e6ca0d49
颜筱辰 Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2025-12-11 18:02:04 -0800

    Add files via upload

---
+++ python/activate_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# choose folder to activate as conda env
+# for folder not in conda path for disk space
+
+home_path="path/to/home"
+conda_path="$home_path/miniconda3"
+conda_load="$conda_path/etc/profile.d/conda.sh"
+env_path="$home_path/path/to/env"
+
+# Load Conda into the shell
+source "$conda_load"
+
+# Activate env
+conda activate "$env_path"
+
+# run in command line
+#source activate_env.sh
+
+# run in command line if file has problems
+# sed -i 's/\r$//' activate_env.sh
+
