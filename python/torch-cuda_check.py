3b983c41690f5c38fb5b2b0b5bcac88a9b20ebe8
颜筱辰 Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2025-12-26 15:53:58 -0800

    Delete python/torch-cuda_check.py

--- python/torch-cuda_check.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-# install torch for cuda 12.2
-# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-
-# check if cuda available
-print(torch.cuda.is_available())
