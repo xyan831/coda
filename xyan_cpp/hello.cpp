d165335fa20b0c13000c7082bdade287bc56c643
颜筱辰 Xiaochen Yan <59631824+xyan831@users.noreply.github.com>
2026-02-02 17:53:16 -0800

    Add files via upload

---
+++ xyan_cpp/hello.cpp
@@ -0,0 +1,20 @@
+// hello world
+
+/* === compile command === */
+// g++ hello.cpp -o run
+/* === debug compile command === */
+// g++ -std=c++17 -Wall -Wextra -pedantic -Weffc++ hello.cpp -o run
+/* === execute command === */
+// .\run
+
+#include <iostream>
+
+//using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
